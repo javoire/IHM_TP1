9d1336512fd7ad1811516d428fbf46038ae04b7b
Jonatan Dahl <jo_da12@hotmail.com>
2012-02-18 22:54:44 +0100

    outputar en mesh. nåt är knas med koordinaterna i .offen...

--- image2mesh.cpp
+++ image2mesh.cpp
@@ -5,35 +5,59 @@
 
 using namespace std;
 
+/* get index */
+int getIndex(int x, int y, int width) {
+	return width*y+x;
+}
+
 int main() {
 
+	/* vars */
 	string version, z, a;
-	int x=0, y=0, index=-1, width, height, depth; // index -1 to skip an empty row in the beginning of the file.... ?!
+	int x=0, y=0, index=-1, width, height, depth, i; // index -1 to skip an empty row in the beginning of the file.... ?!
 
+	/* filestreams */
 	ifstream fin ("image.pgm");
 	ofstream fout ("mesh.off");
 
-	fin >> version >> width >> height >> depth; // img params
+	/* yank imgparams */
+	fin >> version >> width >> height >> depth;
 
-	int indices[width*height];
-	int face[4];
+	/* indexlist + coords */
+	int indices[width*height][2];
 
+	/* write header lines */
 	fout << "OFF" << endl;
 	fout << width*height << " " << (width*height)/3 << " " << "0" << endl;
 
-	while(fin.good()) {
+	/* write 3D coords */
+	while(fin.good() && y < height) {
 		getline(fin, z);
 		if (index > -1) {
 
 			fout << x << " " << y << " " << z << endl;
+			indices[index][0] = x;
+			indices[index][1] = y;
 
 			x += 1; // create the coords
-			if ( x == width - 1 ) { // increase y when x reaches width of image (new row)
+			if ( x == width ) { // increase y when x reaches width of image (new row)
 				x = 0;
 				y += 1;
 			}
 		}
 		index += 1;
+	}
+
+	/* write face-relations */
+	for ( i=0; i<height*width; i++) {
+
+		x = indices[i][0];
+		y = indices[i][1];
+
+		if ( x < width-1 && y < height-1 ) { // don't exceed borders
+			fout << "3 " << getIndex(x, y, width) << " " << getIndex(x, y+1, width) << " " << getIndex(x+1, y, width) << endl;
+			fout << "3 " << getIndex(x, y+1, width) << " " << getIndex(x+1, y+1, width) << " " << getIndex(x+1, y, width) << endl;
+		}
 	}
 
 	fin.close();
